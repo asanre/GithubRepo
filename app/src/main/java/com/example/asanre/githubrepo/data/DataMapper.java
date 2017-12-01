e43e4ef1e47dd603b0331d86eb5ed515f35dcaea
asanre <aser04@gmail.com>
2017-12-01 19:32:53 +0100

    Create data mapper

---
+++ app/src/main/java/com/example/asanre/githubrepo/data/DataMapper.java
@@ -0,0 +1,25 @@
+package com.example.asanre.githubrepo.data;
+
+import com.example.asanre.githubrepo.data.model.RepoEntity;
+import com.example.asanre.githubrepo.data.model.ServiceRepo;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataMapper {
+
+    public static List<RepoEntity> map(List<ServiceRepo> serviceRepos, int page) {
+
+        ModelMapper mapper = new ModelMapper();
+        List<RepoEntity> repoEntities = new ArrayList<>();
+        for (ServiceRepo repo : serviceRepos) {
+            RepoEntity repoEntity = mapper.map(repo, RepoEntity.class);
+            repoEntity.setPage(page);
+            repoEntities.add(repoEntity);
+        }
+
+        return repoEntities;
+    }
+}
