e988e635b099e490c8bc9f8d747c3e980adc2657
Anthony Sanchez Reyes <asanre@MacBook-Air-de-Anthony.local>
2017-12-01 17:19:50 +0100

    init project

---
+++ app/src/main/java/com/example/asanre/githubrepo/GithubApp.java
@@ -0,0 +1,5 @@
+package com.example.asanre.githubrepo;
+
+public class GithubApp {
+
+}
---
+++ app/src/main/java/com/example/asanre/githubrepo/RepoListActivity.java
@@ -0,0 +1,14 @@
+package com.example.asanre.githubrepo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo_list);
+    }
+}
